f08a39773e6106c1b49e09578d7b35e4a426160a
Mert MUSABEŞEOĞLU <193405048@ogrenci.ibu.edu.tr>
2025-01-12 01:59:20 +0300

    Base Page and pom.xml updated.

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,4 +1,30 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+
+    WebDriver driver;
+    By accountUserTextBy = By.className("dropdown-label");
+    By searchBox = By.id("search-form__input-field__search-input");
+
+    public BasePage(WebDriver driver){this.driver = driver;}
+
+    public String getAccountUserText()
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user_1_")));
+        return driver.findElement(accountUserTextBy).getText();
+    }
+
+    public void Search(String Text)
+    {
+        driver.findElement(searchBox).sendKeys(Text+ Keys.ENTER);
+    }
 }
